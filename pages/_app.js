a173e5fbf363cbbdd17c44cb73e2b8f74363c5cc
Calisto184AC0 <arturorajch@gmail.com>
2022-09-04 22:09:11 +0200

    🎉 Inciar con Dolcker

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+    return (
+        <>
+            <Head>
+                <title>
+                    Desarrollo de fachadas ventiladas innovadoras y de alto
+                    rendimiento | Dolcker
+                </title>
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1.0'
+                />
+                <meta
+                    name='description'
+                    content='Dolcker trabaja con arquitectos y diseñadores para llevar sus proyectos a la vanguardia de la construcción sostenible. Nuestra apuesta decidida por la calidad de nuestros productos nos hace merecedores de la confianza de un gran número de arquitectos, colocadores y clientes.'
+                />
+                <meta
+                    name='keywords'
+                    content='fachada, fachada ventilada, porcelánico, arquitectura, diseño'
+                />
+                <meta name='robots' content='index' />
+                <meta charSet='UTF-8' />
+                <link rel='icon' type='image/svg+xml' href='favicon.ico' />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
